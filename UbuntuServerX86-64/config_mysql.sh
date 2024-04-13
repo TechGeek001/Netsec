055656a94931dec8b44a94065cd1b09876ba1cd2
Andrew Zitter <azitter@gmail.com>
2024-04-13 19:48:38 +0000

    update

--- UbuntuServerX86-64/config_mysql.sh
+++
@@ -1,7 +0,0 @@
-mysql
-echo "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypass';"
-echo "CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypass';"
-echo "GRANT ALL ON *.* TO 'myuser'@'localhost';"
-echo "GRANT ALL ON *.* TO 'myuser'@'%';"
-echo "FLUSH PRIVILEGES;"
-echo "exit"
