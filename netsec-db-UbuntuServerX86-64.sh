ae3c7c7f3dc1d88c84c98bab6e2d3611e63a578d
Andrew Zitter <azitter@gmail.com>
2024-04-13 18:44:05 +0000

    update

--- netsec-db-UbuntuServerX86-64.sh
+++ netsec-db-UbuntuServerX86-64.sh
@@ -32,6 +32,8 @@ echo 'domains.txt' >> .git/info/sparse-checkout
 git pull origin main
 
 echo 'cd into UbuntuServerX86-64 and view details in Dockerfile for building, running, and attaching to the container'
+echo 'docker build -t tnetsecdb .'
+echo 'docker run -d --name netsecdb --dns 192.168.25.10 --dns-search netsec-docker.isi.jhu.edu --privileged --network host tnetsecdb:latest'
 
 # References:
 #   https://terminalroot.com/how-to-clone-only-a-subdirectory-with-git-or-svn
