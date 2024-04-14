b2a8d7694defcc6c2cf2c9c44e0c0106079a122e
Andrew <azitter@gmail.com>
2024-04-14 01:12:42 -0400

    Update eadu.sql

--- mysqldb/UbuntuServerX86-64/eadu.sql
+++ mysqldb/UbuntuServerX86-64/eadu.sql
@@ -6,22 +6,22 @@ CREATE TABLE IF NOT EXISTS users (
 	password VARCHAR(32),
 	remarks VARCHAR(255)
 );
-INSERT INTO users (name, password) VALUES (
+INSERT INTO users (name, password, remarks) VALUES (
 "Galen Erso",
 "starDUST",
 "Executed // Reason: Traitor to the Empire"
 );
-INSERT INTO users (name, password) VALUES (
+INSERT INTO users (name, password, remarks) VALUES (
 "Kendal Ozzel",
 "tooCLOSEtoHOTH",
 "Executed // Reason: Gross Incompetence"
 );
-INSERT INTO users (name, password) VALUES (
+INSERT INTO users (name, password, remarks) VALUES (
 "Orson Krennic",
 "IamREMARKABLE",
 "Deceased // Reason: Death Star Strike on Scarif"
 );
-INSERT INTO users (name, password) VALUES (
+INSERT INTO users (name, password, remarks) VALUES (
 "Maximilian Veers",
 "startYOURlanding",
 "Active"
